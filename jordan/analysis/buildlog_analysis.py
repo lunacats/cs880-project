1fb568fe231ab806c7b3192569723b4be48a87f9
Jordan Chadwick <jordan.chadwick@gmail.com>
2019-04-27 21:57:06 -0400

    buildlog_analysis

--- jordan/analysis/buildlog_analysis.py
+++ jordan/analysis/buildlog_analysis.py
@@ -2,7 +2,7 @@
 #
 # usage: buildlog_analysis.py <directory>
 #
-# 
+
 
 import os
 import sys
@@ -21,11 +21,27 @@ def main():
     files = os.listdir(directory)
     
     # traverse the files and analyze the build log contents
+    file_gcc_lines = {}
     for f in files:
         file_path = os.path.join(directory, f)
         with open(file_path, 'r') as fd:
-            fd.
+            raw_text = fd.read()
+            lines = raw_text.splitlines()
+
+        # process line-by-line
+        gcc_lines = []
+        for l in lines:
+            if 'gcc' in l[0:3]:
+                gcc_lines.append(l)
+
+        file_gcc_lines[f] = gcc_lines
+
+    # analyze the gcc lines for each file
+    print("lines:")
+    for k in sorted(file_gcc_lines):
+        print("%s = %s" % (k, file_gcc_lines[k]))
+
     
 
 if __name__ == "__main__":
-	main()
+    main()
