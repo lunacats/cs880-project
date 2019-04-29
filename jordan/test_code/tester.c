c60b6e59c1ff87317fd588295e87eb52f03fa5d6
Jordan Chadwick <jordan.chadwick@gmail.com>
2019-04-29 18:56:48 -0400

    tester

--- jordan/test_code/tester.c
+++ jordan/test_code/tester.c
@@ -11,12 +11,16 @@ Jordan Chadwick
 #include <stdio.h>
 
 // read_data function
-void read_data_buffer(FILE *fp) {
+void read_file(FILE *fp) {
     int buff_size = 255;
     char buff[buff_size];
 
-    fgets(buff, buff_size, fp);
+    if (fgets(buff, buff_size, fp) == NULL);
+        return -1;
+
     printf("buff = %s\n", buff);
+
+    return buff_size;
 }
 
 // main function
@@ -24,7 +28,11 @@ int main(int argc, char const *argv[]) {
 
     FILE *fp;
     fp = fopen(argv[1], "r");
-    read_data_buffer(fp);
+    
+    chars_read = read_file(fp);
+    while(chars_read != -1) {
+        chars_read = read_file(fp)
+    }
 
     return 0;
 }
