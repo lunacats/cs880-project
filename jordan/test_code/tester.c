923bda98ab2871ebe93a042ec957c5218f73f94c
Jordan Chadwick <jordan.chadwick@gmail.com>
2019-04-29 18:36:55 -0400

    tester

---
+++ jordan/test_code/tester.c
@@ -0,0 +1,29 @@
+/*
+tester.c
+
+Tester for stack protection mechanisms.
+
+CS 880
+Jordan Chadwick
+*/
+
+#include <stdlib.h>
+
+// read_data function
+void read_data_buffer(FILE *fp) {
+    int buff_size = 255;
+    char buff[buff_size];
+
+    fgets(buff, buff_size, fp);
+    print("buff = %s\n", buff);
+}
+
+// main function
+int main(int argc, char const *argv[]) {
+
+    FILE *fp;
+    fp = fopen(argv[1], "r");
+    read_data_buffer(fp);
+
+    return 0;
+}
