d8fde99a5f21d8070d0b09fc2b8fd27a996d3c62
Jordan Chadwick <jordan.chadwick@gmail.com>
2019-05-02 21:13:29 -0400

    static_analysis

--- jordan/static_analysis/static_analysis.py
+++ jordan/static_analysis/static_analysis.py
@@ -36,6 +36,7 @@ def main():
     file_path = args.file_path
     verbose = args.verbose
     
+    print("file path = %s" % file_path)
     if not os.path.isfile(file_path):
         print("Invalid file path, file does not exist or is a directory")
         sys.exit(2)
