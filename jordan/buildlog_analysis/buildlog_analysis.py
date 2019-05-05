2f71198e12630be8bcf2935eb08fd90715bf30ae
Jordan Chadwick <jordan.chadwick@gmail.com>
2019-05-05 14:35:50 -0400

    buildlog analysis

--- jordan/buildlog_analysis/buildlog_analysis.py
+++ jordan/buildlog_analysis/buildlog_analysis.py
@@ -52,7 +52,7 @@ def main():
         num_stack_protector = 0
         num_stack_protector_strong = 0
         num_stack_protector_all = 0
-        num_no_stack_protection = 0
+        num_no_stack_protector = 0
         for gcc in file_gcc_lines[k]:
             if '-fstack-protector ' in gcc:
                 num_stack_protector += 1
